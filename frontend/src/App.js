ab864277d2291a054eed522500db82174d62331c
marlonanthony <mad1083@gmail.com>
2019-05-10 20:11:28 -0400

    added login and linked to backend graphql api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom' 
 
 import AuthPage from './pages/Auth'
 import BookingsPage from './pages/Bookings'
 import EventsPage from './pages/Events'
+import MainNavigation from './components/navigation/MainNavigation'
+import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from='/' to='/auth' exact/>
-        <Route path='/auth' component={AuthPage} />
-        <Route path='/events' component={EventsPage} />
-        <Route path='/bookings' component={BookingsPage} />
-      </Switch>
+      <Fragment>
+        <MainNavigation />
+        <main className='main-content'>
+          <Switch>
+            <Redirect from='/' to='/auth' exact/>
+            <Route path='/auth' component={AuthPage} />
+            <Route path='/events' component={EventsPage} />
+            <Route path='/bookings' component={BookingsPage} />
+          </Switch>
+        </main>
+      </Fragment>
     </BrowserRouter>
   )
 }
