6f8faa9a59ecc298fb4a2ebb4bd6fd4303e15e62
marlonanthony <mad1083@gmail.com>
2019-05-11 21:49:45 -0400

    added add event modal

--- frontend/src/components/navigation/MainNavigation.js
+++ frontend/src/components/navigation/MainNavigation.js
@@ -14,15 +14,18 @@ const MainNavigation = props => (
                     </div>
                     <nav className='main-navigation__items'>
                         <ul>
-                            { !context.token && <li>
-                                <NavLink to='/auth'>Authenticate</NavLink>
-                            </li> }
-                            <li>
-                                <NavLink to='/events'>Events</NavLink>
-                            </li>
-                            { context.token && <li>
-                                <NavLink to='/bookings'>Bookings</NavLink>
-                            </li> }
+                            { !context.token && (
+                                <li><NavLink to='/auth'>Authenticate</NavLink></li> 
+                            )}
+
+                            <li><NavLink to='/events'>Events</NavLink></li>
+
+                            { context.token && (
+                                <React.Fragment>
+                                    <li><NavLink to='/bookings'>Bookings</NavLink></li> 
+                                    <li><button onClick={context.logout}>Logout</button></li>
+                                </React.Fragment>
+                            )}
                         </ul>
                     </nav>
                 </header>
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,46 @@
-import React, { Component } from 'react' 
+import React, { Component, Fragment } from 'react' 
+
+import Modal from '../components/Modal/Modal'
+import Backdrop from '../components/backdrop/Backdrop'
+import './Events.css'
 
 class EventsPage extends Component {
+    state = { creating: false }
+
+    startCreateEventHandler = () => {
+        this.setState({ creating: true })
+    }
+
+    modalConfirmHandler = () => {
+        this.setState({ creating: false })
+    }
+
+    modalCancelHandler = () => {
+        this.setState({ creating: false })
+    }
+
     render() {
-        return (<h1>Events Page</h1>)
+        const { creating } = this.state 
+        return (
+            <Fragment>
+                { creating && <Backdrop />}
+                { creating && (
+                    <Modal 
+                        title='Add Event' 
+                        canCancel 
+                        canConfirm 
+                        onCancel={this.modalCancelHandler} 
+                        onConfirm={this.modalConfirmHandler}
+                    >
+                        <p>Modal Content</p>
+                    </Modal>
+                )}
+                <div className='events-control'>
+                    <p>Share your events</p>
+                    <button className='btn' onClick={this.startCreateEventHandler}>Create Event</button>
+                </div>
+            </Fragment>
+        )
     }
 }
 
